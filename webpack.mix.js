59cbcce9224bfda343b1558f914286fcfb36b2bc
Kirill Gugaev <gugaevkirill@gmail.com>
2017-05-11 21:01:41 +0300

    Пофиксил баг верстки

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,18 +13,6 @@ var path = require('path');
  */
 
 mix.copy('resources/assets/css/bootstrap.min.css', 'public/css')
-    .copy('resources/assets/css/font-awesome.min.css', 'public/css')
-    .copy('resources/assets/css/idangerous.swiper.css', 'public/css')
-    .copy('resources/assets/css/style.css', 'public/css')
-    .copy('resources/assets/js/global.js', 'public/js')
-    .copy('resources/assets/js/idangerous.swiper.min.js', 'public/js')
-    .copy('resources/assets/js/isotope.pkgd.min.js', 'public/js')
-    .copy('resources/assets/js/jquery.jscrollpane.min.js', 'public/js')
-    .copy('resources/assets/js/jquery.mousewheel.js', 'public/js')
-    .copy('resources/assets/js/jquery-2.1.3.min.js', 'public/js')
-    .copy('resources/assets/js/jquery-ui.min.js', 'public/js')
-    .copy('resources/assets/js/map.js', 'public/js')
-    .js('resources/assets/js/app.js', 'public/js')
     .webpackConfig({
         resolve: {
             modules: [
