2de9ab6b8d289272664ee01390b117504dc0f787
Kirill Gugaev <gugaevkirill@gmail.com>
2017-06-28 00:21:10 +0300

    Name up to 150 chars

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -28,7 +28,7 @@ class ProductRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
             'order' => 'required|min:0|numeric',
             'brand_slug' => 'required',
             'category_slug' => 'required',
-            'name' => 'required|string|min:5|max:100',
+            'name' => 'required|string|min:5|max:150',
             'description' => 'string|min:10|max:10000',
             'price_rub' => 'numeric|min:100|max:10000000000',
             'price_dollar' => 'numeric|min:100|max:10000000000',
