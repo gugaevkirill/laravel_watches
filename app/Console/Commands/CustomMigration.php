d4c3a20b72d520a07bdcaa79fe73c342c4882489
Kirill Gugaev <gugaevkirill@gmail.com>
2017-08-27 13:32:46 +0300

    Очистить миграцию

--- app/Console/Commands/CustomMigration.php
+++ app/Console/Commands/CustomMigration.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Catalog\Param;
-use App\Models\Catalog\Product;
 use Illuminate\Console\Command;
 
 class CustomMigration extends Command
@@ -34,30 +32,6 @@ class CustomMigration extends Command
      */
     public function handle()
     {
-        $params = Param::where('type', Param::TYPE_STR)->get();
-        foreach (Product::all() as $product) {
-            if (!$attrs = $product->attrs) {
-                continue;
-            };
-
-            foreach ($params as $param) {
-                if (!array_key_exists($param->slug, $attrs)) {
-                    continue;
-                }
-
-                $old = $attrs[$param->slug];
-                $attrs[$param->slug] = [
-                    'ru' => $old,
-                    'en' => $old,
-                ];
-
-                echo '.';
-            }
-            $product->attrs = $attrs;
-
-            echo "\n";
-
-            $product->save();
-        }
+        // Yout code here
     }
 }
